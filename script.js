dec79f639e3db1209b2eb54988e7e145200ee21e
yassinexcv <yassinexcv188@gmail.com>
2022-03-18 17:05:35 +0100

    counter

---
+++ script.js
@@ -0,0 +1,16 @@
+new Vue ({
+    el:'#app',
+    data:{
+       counter:0
+    
+   
+    },
+    methods:{
+      increment:function(){
+          this.counter++;
+      },
+      increment:function(){
+          this.counter--;
+      }
+    }
+})
